4cc4f29097392117b536ece857637a49133bba61
hain9563 <32196586+hain9563@users.noreply.github.com>
2020-06-22 10:23:52 +0900

    render pug file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("logout");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+export const users = (req, res) => res.render("Users");
+export const userDetail = (req, res) => res.render("User detail");
+export const editProfile = (req, res) => res.render("Edit profile");
+export const changePassword = (req, res) => res.render("Change password");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home");
+export const search = (req, res) => res.render("Search");
+export const videos = (req, res) => res.render("videos");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("video detail");
+export const editVideo = (req, res) => res.render("edit video");
+export const deleteVideo = (req, res) => res.render("delete video");
