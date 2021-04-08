50e69ed133d581a164bcd01f35e31f0e902e7ec7
hain9563 <32196586+hain9563@users.noreply.github.com>
2021-04-08 18:49:13 +0900

    bodyParser 추가

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import { urlencoded, json } from "body-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
-import { localsMiddleWare } from "./middlewares";
-
 const app = express();
 
 app.use(helmet());
+app.use(function(req, res, next){
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    return next();
+});
 app.set("view engine", "pug");
-app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(localsMiddleWare);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
