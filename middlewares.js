ca263ac4dc62fa6a304daff5dc3d5710b1f5cf40
hain9563 <32196586+hain9563@users.noreply.github.com>
2021-04-08 18:30:08 +0900

    user login 후 html 확인을 위한 local data 추가

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,13 @@
 import routes from "./routes";
-export const localsMiddleWare = (req, res, next) => {
+
+export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  }
   next();
 };
+
+
