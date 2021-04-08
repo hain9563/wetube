35002f8eca8a938224a007ce62c41b2c569a28b7
hain9563 <32196586+hain9563@users.noreply.github.com>
2021-04-08 18:28:40 +0900

    포트 4000으로 변경

--- init.js
+++ init.js
@@ -1,7 +1,8 @@
 import app from "./app";
 
-const PORT = 5000;
+const PORT = 4000;
 
-const handelListening = () => console.log("Listening on : 4000");
+const handleListening = () =>
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
 
-app.listen(PORT, handelListening);
+app.listen(PORT, handleListening);
